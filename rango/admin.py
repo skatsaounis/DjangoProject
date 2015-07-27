f55cdebd8f587be37a3df18ac32160c504b3ab62
stammy31 <katsaouniss@gmail.com>
2015-07-27 17:02:18 +0300

    User Registration added. Registration Form added.

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,7 @@
 # Register your models here.
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
@@ -8,5 +9,7 @@ class PageAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
+admin.site.register(UserProfile)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+
