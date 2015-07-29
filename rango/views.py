4aa488a8a27514f934b78bfed7fbb0bd07e6b05f
stammy31 <katsaouniss@gmail.com>
2015-07-29 10:33:06 +0300

    Restricted Page Added. Logout Functionality added. Redirection of not logged in users added.

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-
 # Import the Category model and Page model
 from rango.models import Category, Page
-
 from rango.forms import CategoryForm, PageForm
-
 from rango.forms import UserForm, UserProfileForm
-
-from django.contrib.auth import authenticate, login
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 
@@ -227,4 +222,18 @@ def user_login(request):
         # blank dictionary object...
         return render(request, 'rango/login.html', {})
 
+
+# Only logged in users can see this
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+# Use the login_required() decorator to ensure only those logged in can access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/rango/')
 
